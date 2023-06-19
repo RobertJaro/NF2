75c588a70db39380651cf0ce2dfa9462901abba4
RobertJaro <robert.jarolim@gmail.com>
2023-06-19 09:38:20 +0200

    update setup configuration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='nf2',
-    version='v0.2',
+    version='v0.3',
     packages=setuptools.find_packages(),
     url='https://github.com/RobertJaro/NF2',
     license='GNU GENERAL PUBLIC LICENSE',
@@ -11,5 +11,5 @@ setup(
     author_email='',
     description='Neural Network Force Free magnetic field extrapolation',
     install_requires=['torch>=1.8', 'sunpy>=3.0', 'scikit-image', 'scikit-learn', 'tqdm',
-                      'numpy', 'matplotlib', 'astropy', 'drms'],
+                      'numpy', 'matplotlib', 'astropy', 'drms', 'wandb>=0.13', 'lightning==1.9.3'],
 )
