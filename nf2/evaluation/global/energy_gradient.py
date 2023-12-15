update script for derecho cluster, update download for disambiguation, temporarily remove multi-height, add evaluation, fix boundary callback, add test data modules, remove asinh strech for gradient regularization,

nf2/evaluation/global/energy_gradient.py
@@ -11,8 +11,8 @@ from torch import nn
 from nf2.data.util import spherical_to_cartesian
 from nf2.evaluation.unpack import load_coords
 
-files = '/gpfs/gpfs0/robert.jarolim/full_disk/2154_vp_series_v1'
-results_path = '/gpfs/gpfs0/robert.jarolim/full_disk/2154_vp_series_v1/results'
+files = '/glade/work/rjarolim/global/2154_vp_v4'
+results_path = '/glade/work/rjarolim/global/2154_vp_v4/results'
 os.makedirs(results_path, exist_ok=True)
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')

nf2/extrapolate.py
@@ -8,10 +8,10 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, LambdaCallback
 from pytorch_lightning.loggers import WandbLogger
 
-from nf2.train.callback import SlicesCallback
+from nf2.train.callback import SlicesCallback, BoundaryCallback
 from nf2.train.module import NF2Module, save
 from nf2.train.data_loader import NumpyDataModule, SOLISDataModule, FITSDataModule, AnalyticDataModule, SHARPDataModule, \
-    SphericalDataModule, AzimuthDataModule
+    SphericalDataModule, SynopticDataModule, PotentialTestDataModule, AzimuthDataModule
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, required=True,
@@ -43,6 +43,8 @@ if wandb_id is not None:
     shutil.move(os.path.join(base_path, 'model.ckpt'), os.path.join(base_path, 'last.ckpt'))
     args.data['plot_overview'] = False  # skip overview plot for restored model
 
+callbacks = []
+
 if 'work_directory' not in args.data or args.data['work_directory'] is None:
     args.data['work_directory'] = base_path
 if args.data["type"] == 'numpy':
@@ -59,11 +61,18 @@ elif args.data["type"] == 'spherical':
     data_module = SphericalDataModule(**args.data, plot_settings=args.plot)
 elif args.data["type"] == 'azimuth':
     data_module = AzimuthDataModule(**args.data, plot_settings=args.plot)
+    boundary_callback = BoundaryCallback(data_module.cube_shape)
+    callbacks += [boundary_callback]
+elif args.data["type"] == 'synoptic':
+    data_module = SynopticDataModule(**args.data, plot_settings=args.plot)
+elif args.data["type"] == 'potential_test':
+    data_module = PotentialTestDataModule(**args.data, plot_settings=args.plot)
 else:
     raise NotImplementedError(f'Unknown data loader {args.data["type"]}')
 
 plot_slices_callbacks = [SlicesCallback(plot_settings['name'], data_module.slices_datasets[plot_settings['name']].cube_shape)
                          for plot_settings in args.plot if plot_settings['type'] == 'slices']
+# boundary_callback = BoundaryCallback(data_module.cube_shape)
 
 validation_settings = {'cube_shape': data_module.cube_dataset.coords_shape,
                        'gauss_per_dB': args.data["b_norm"],
@@ -75,7 +84,7 @@ nf2 = NF2Module(validation_settings, **args.model, **args.training)
 
 config = {'data': args.data, 'model': args.model, 'training': args.training}
 save_callback = LambdaCallback(
-    on_validation_end=lambda *args: save(save_path, nf2.model, data_module, config, nf2.coordinate_transform))
+    on_validation_end=lambda *args: save(save_path, nf2.model, data_module, config))
 checkpoint_callback = ModelCheckpoint(dirpath=base_path,
                                       every_n_train_steps=args.training["validation_interval"] if "validation_interval" in args.training else None,
                                       every_n_epochs=args.training['check_val_every_n_epoch'] if 'check_val_every_n_epoch' in args.training else None,
@@ -83,6 +92,7 @@ checkpoint_callback = ModelCheckpoint(dirpath=base_path,
 
 torch.set_float32_matmul_precision('medium')  # for A100 GPUs
 n_gpus = torch.cuda.device_count()
+callbacks += [checkpoint_callback, save_callback, *plot_slices_callbacks]
 trainer = Trainer(max_epochs=int(args.training['epochs']) if 'epochs' in args.training else 1,
                   logger=wandb_logger,
                   devices=n_gpus if n_gpus > 0 else None,
@@ -92,9 +102,9 @@ trainer = Trainer(max_epochs=int(args.training['epochs']) if 'epochs' in args.tr
                   val_check_interval=int(args.training['validation_interval']) if 'validation_interval' in args.training else None,
                   check_val_every_n_epoch=args.training['check_val_every_n_epoch'] if 'check_val_every_n_epoch' in args.training else None,
                   gradient_clip_val=0.1,
-                  callbacks=[checkpoint_callback, save_callback, *plot_slices_callbacks])
+                  callbacks=callbacks)
 
 trainer.fit(nf2, data_module, ckpt_path='last')
-save(save_path, nf2.model, data_module, config, coordinate_transform_module=nf2.coordinate_transform)
+save(save_path, nf2.model, data_module, config)
 # clean up
 data_module.clear()

nf2/extrapolate_series.py
@@ -4,6 +4,7 @@ import json
 import os
 
 import torch
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LambdaCallback
 from pytorch_lightning.loggers import WandbLogger
@@ -45,7 +46,7 @@ data_paths = list(zip(hmi_p_files, err_p_files, hmi_t_files, err_t_files, hmi_r_
 data_paths = [{'Bp': d[0], 'Bp_err': d[1], 'Bt': d[2], 'Bt_err': d[3], 'Br': d[4], 'Br_err': d[5]} for d in data_paths]
 # reload model training
 ckpts = sorted(glob.glob(os.path.join(base_path, '*.nf2')))
-meta_path = ckpts[-1] if len(ckpts) > 0 else args.meta_path  # reload last savepoint
+ckpt_path = 'last' if len(ckpts) > 0 else args.meta_path  # reload last savepoint
 data_paths = data_paths[len(ckpts):]  # select remaining extrapolations
 
 if 'work_directory' not in args.data or args.data['work_directory'] is None:
@@ -66,7 +67,7 @@ validation_settings = {'cube_shape': data_module.cube_dataset.coords_shape,
                        'names': [plot_settings['name'] for plot_settings in args.plot],
                        }
 
-nf2 = NF2Module(validation_settings, meta_path=meta_path, **args.model, **args.training)
+nf2 = NF2Module(validation_settings, **args.model, **args.training)
 
 
 reload_dataloaders_every_n_epochs = args.training['reload_dataloaders_every_n_epochs'] if 'reload_dataloaders_every_n_epochs' in args.training else 1
@@ -74,19 +75,23 @@ reload_dataloaders_every_n_epochs = args.training['reload_dataloaders_every_n_ep
 # callback
 config = {'data': args.data, 'model': args.model, 'training': args.training}
 save_callback = LambdaCallback(on_train_epoch_end=lambda *args: save(
-    os.path.join(base_path, os.path.basename(data_paths[nf2.current_epoch // reload_dataloaders_every_n_epochs]['Br']).split('.')[-3] + '.nf2'),
-    nf2.model, data_module, config, nf2.coordinate_transform))
+    os.path.join(base_path, os.path.basename(data_module.current_files['Br']).split('.')[-3] + '.nf2'),
+    nf2.model, data_module, config))
+
+checkpoint_callback = ModelCheckpoint(dirpath=base_path,
+                                      every_n_epochs=args.training['check_val_every_n_epoch'] if 'check_val_every_n_epoch' in args.training else 1,
+                                      save_last=True)
 
 # general training parameters
 torch.set_float32_matmul_precision('medium')  # for A100 GPUs
 n_gpus = torch.cuda.device_count()
 
-trainer = Trainer(max_epochs=len(data_paths) * reload_dataloaders_every_n_epochs,
+trainer = Trainer(max_epochs=-1,
                   logger=wandb_logger,
                   devices=n_gpus,
                   accelerator='gpu' if n_gpus >= 1 else None,
                   strategy='dp' if n_gpus > 1 else None,  # ddp breaks memory and wandb
-                  num_sanity_val_steps=0, callbacks=[save_callback, *plot_slices_callbacks],
+                  num_sanity_val_steps=0, callbacks=[save_callback, checkpoint_callback, *plot_slices_callbacks],
                   gradient_clip_val=0.1, reload_dataloaders_every_n_epochs=reload_dataloaders_every_n_epochs,
                   check_val_every_n_epoch=args.training['check_val_every_n_epoch'] if 'check_val_every_n_epoch' in args.training else 1, )
-trainer.fit(nf2, data_module)
+trainer.fit(nf2, data_module, ckpt_path=ckpt_path)

nf2/train/callback.py
@@ -1,5 +1,6 @@
 import numpy as np
 import wandb
+from astropy.nddata import block_reduce
 from pytorch_lightning import Callback
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
@@ -112,7 +113,6 @@ class BoundaryCallback(Callback):
     def plot_b(self, b, b_true, coords):
         fig, axs = plt.subplots(3, 2, figsize=(8, 12))
 
-        b_norm = 0.2
         extent = [coords[:, 2].min(), coords[:, 2].max(), coords[:, 1].min(), coords[:, 1].max()]
         extent = np.rad2deg(extent)
 
@@ -120,46 +120,53 @@ class BoundaryCallback(Callback):
         x = coords[:, 2]
         y = coords[:, 1]
 
-        x = ((x - x.min()) / (x.max() - x.min()) * (boundary_shape[1] - 1)).astype(int)
-        y = ((y - y.min()) / (y.max() - y.min()) * (boundary_shape[0] - 1)).astype(int)
+        x = np.round((x - x.min()) / (x.max() - x.min()) * (boundary_shape[1] - 1)).astype(int)
+        y = np.round((y - y.min()) / (y.max() - y.min()) * (boundary_shape[0] - 1)).astype(int)
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b[:, 0]
-        im = axs[0, 0].imshow(img, cmap='gray', vmin=-b_norm, vmax=b_norm, origin='lower', extent=extent)
+        img = block_reduce(img, (8, 8), np.nanmean)
+        b_norm = np.max(np.abs(img))
+        im = axs[0, 0].imshow(img, cmap='gray', vmin=-b_norm, vmax=b_norm, origin='lower', extent=extent, interpolation='nearest')
         divider = make_axes_locatable(axs[0, 0])
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im, cax=cax, label='B_LOS [G]')
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b_true[:, 0]
+        img = block_reduce(img, (8, 8), np.nanmean)
         im = axs[0, 1].imshow(img, cmap='gray', vmin=-b_norm, vmax=b_norm, origin='lower', extent=extent)
         divider = make_axes_locatable(axs[0, 1])
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im, cax=cax, label='B_LOS [G]')
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b[:, 1]
+        img = block_reduce(img, (8, 8), np.nanmean)
         im = axs[1, 0].imshow(img, vmin=0, vmax=b_norm, cmap='gray', origin='lower', extent=extent)
         divider = make_axes_locatable(axs[1, 0])
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im, cax=cax, label='B_TRV [G]')
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b_true[:, 1]
+        img = block_reduce(img, (8, 8), np.nanmean)
         im = axs[1, 1].imshow(img, vmin=0, vmax=b_norm, cmap='gray', origin='lower', extent=extent)
         divider = make_axes_locatable(axs[1, 1])
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im, cax=cax, label='B_TRV [G]')
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b[:, 2]
+        img = block_reduce(img, (8, 8), np.nanmean)
         im = axs[2, 0].imshow(img, vmin=-np.pi, vmax=np.pi, cmap='gray', origin='lower', extent=extent)
         divider = make_axes_locatable(axs[2, 0])
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im, cax=cax, label='Azimuth [rad]')
 
-        img = np.zeros(boundary_shape)
+        img = np.zeros(boundary_shape) * np.nan
         img[y, x] = b_true[:, 2]
+        img = block_reduce(img, (8, 8), np.nanmean)
         im = axs[2, 1].imshow(img, vmin=-np.pi, vmax=np.pi, cmap='gray', origin='lower', extent=extent)
         divider = make_axes_locatable(axs[2, 1])
         cax = divider.append_axes("right", size="5%", pad=0.05)

nf2/train/loss.py
@@ -43,7 +43,7 @@ class DivergenceLoss(nn.Module):
 
 class RadialLoss(nn.Module):
 
-    def __init__(self, base_radius=1.3):
+    def __init__(self, base_radius=1.0):
         super().__init__()
         self.base_radius = base_radius
 
@@ -84,15 +84,14 @@ class PotentialLoss(nn.Module):
         j = torch.stack([rot_x, rot_y, rot_z], -1)
         potential_loss = torch.sum(j ** 2, dim=-1)
 
-        radial_weight = torch.sqrt(torch.sum(coords ** 2, dim=-1) + 1e-7) if self.radial_weight else 1
+        radial_weight = torch.sqrt(torch.sum(coords ** 2, dim=-1) + 1e-7) - 1 if self.radial_weight else 1
 
         return (potential_loss * radial_weight).mean()
 
 class EnergyGradientLoss(nn.Module):
 
-    def __init__(self, base_radius=1.3):
+    def __init__(self, base_radius=1.0):
         super().__init__()
-        self.asinh_stretch = nn.Parameter(torch.tensor(np.arcsinh(1e3)))
         self.base_radius = base_radius
 
     def forward(self, b, jac_matrix, coords, n_boundary_coords=None, *args, **kwargs):
@@ -125,6 +124,7 @@ class EnergyGradientLoss(nn.Module):
 
         sampled_dE_dr = dE_dr[n_boundary_coords:]
         energy_gradient_regularization = torch.relu(sampled_dE_dr) * radius_weight
+        energy_gradient_regularization = torch.asinh(energy_gradient_regularization * 1e3) / self.asinh_stretch
         energy_gradient_regularization = energy_gradient_regularization.mean()
 
         return energy_gradient_regularization
@@ -140,23 +140,36 @@ class NaNLoss(nn.Module):
 
 class AzimuthBoundaryLoss(nn.Module):
 
+    def __init__(self, disambiguate=True):
+        super().__init__()
+        self.disambiguate = disambiguate
+
     def forward(self, boundary_b, b_true, transform=None, *args, **kwargs):
         # apply transforms
-        transformed_b = torch.einsum('ijk,ik->ij', transform, boundary_b) if transform is not None else boundary_b
-        transformed_b = img_to_los_trv_azi(transformed_b)
+        b_pred = torch.einsum('ijk,ik->ij', transform, boundary_b) if transform is not None else boundary_b
+        b_pred = img_to_los_trv_azi(b_pred)
 
         bz_true = b_true[:, 0]
-        bx_true = b_true[:, 1] * torch.abs(torch.sin(b_true[:, 2]))
-        by_true = b_true[:, 1] * torch.abs(torch.cos(b_true[:, 2]))
+
+        if self.disambiguate:
+            bx_true = b_true[:, 1] * torch.abs(torch.sin(b_true[:, 2]))
+            by_true = b_true[:, 1] * torch.abs(torch.cos(b_true[:, 2]))
+        else:
+            bx_true = b_true[:, 1] * torch.sin(b_true[:, 2])
+            by_true = b_true[:, 1] * torch.cos(b_true[:, 2])
         b_true = torch.stack([bx_true, by_true, bz_true], -1)
 
-        bz = transformed_b[:, 0]
-        bx = transformed_b[:, 1] * torch.abs(torch.sin(transformed_b[:, 2]))
-        by = transformed_b[:, 1] * torch.abs(torch.cos(transformed_b[:, 2]))
-        transformed_b = torch.stack([bx, by, bz], -1)
+        bz = b_pred[:, 0]
+        if self.disambiguate:
+            bx = b_pred[:, 1] * torch.abs(torch.sin(b_pred[:, 2]))
+            by = b_pred[:, 1] * torch.abs(torch.cos(b_pred[:, 2]))
+        else:
+            bx = b_pred[:, 1] * torch.sin(b_pred[:, 2])
+            by = b_pred[:, 1] * torch.cos(b_pred[:, 2])
+        b_pred = torch.stack([bx, by, bz], -1)
 
         # compute diff
-        b_diff = torch.abs(transformed_b - b_true)
+        b_diff = torch.abs(b_pred - b_true)
         b_diff = torch.mean(torch.nansum(b_diff.pow(2), -1))
 
         return b_diff
@@ -167,7 +180,7 @@ def img_to_los_trv_azi(transformed_b):
     cos_inc = transformed_b[..., 2] / (fld + eps)
     azi = torch.arctan2(transformed_b[..., 0], -transformed_b[..., 1] + eps)
     B_los = fld * cos_inc
-    B_trv = torch.abs(fld * (1 - cos_inc ** 2) ** 0.5)
+    B_trv = fld * (1 - cos_inc ** 2) ** 0.5
     transformed_b = torch.stack([B_los, B_trv, azi], -1)
     return transformed_b
 
@@ -193,42 +206,27 @@ class HeightLoss(nn.Module):
 
         return height_regularization
 
-class BDiffMetric(nn.Module):
+class SphericalTransform(nn.Module):
 
-    def forward(self, b, b_true, b_err=0, transform=None):
+    def forward(self, b, b_true, transform=None):
         b = torch.einsum('ijk,ik->ij', transform, b) if transform is not None else b
-
-        # compute boundary loss
-        b_diff_error = torch.clip(torch.abs(b - b_true) - b_err, 0)
-        b_diff_error = torch.mean(torch.nansum(b_diff_error.pow(2), -1).pow(0.5))
-
-        b_diff = torch.abs(b - b_true)
-        b_diff = torch.mean(torch.nansum(b_diff.pow(2), -1).pow(0.5))
-
-        return b_diff, b_diff_error
+        return b, b_true
 
 
-class AziDiffMetric(nn.Module):
+class AzimuthTransform(nn.Module):
 
-    def forward(self, b, b_true, b_err=0, transform=None):
+    def forward(self, b, b_true, transform=None):
         b = torch.einsum('ijk,ik->ij', transform, b) if transform is not None else b
         b = img_to_los_trv_azi(b)
 
-        bz_true = b_true[:, 0]
-        bx_true = b_true[:, 1] * torch.sin(b_true[:, 2])
-        by_true = b_true[:, 1] * torch.cos(b_true[:, 2])
-        b_true = torch.stack([bx_true, by_true, bz_true], -1)
+        # bz_true = b_true[:, 0]
+        # bx_true = b_true[:, 1] * torch.sin(b_true[:, 2])
+        # by_true = b_true[:, 1] * torch.cos(b_true[:, 2])
+        # b_true = torch.stack([bx_true, by_true, bz_true], -1)
+        #
+        # bz = b[:, 0]
+        # bx = b[:, 1] * torch.sin(b[:, 2])
+        # by = b[:, 1] * torch.cos(b[:, 2])
+        # b = torch.stack([bx, by, bz], -1)
 
-        bz = b[:, 0]
-        bx = b[:, 1] * torch.sin(b[:, 2])
-        by = b[:, 1] * torch.cos(b[:, 2])
-        transformed_b = torch.stack([bx, by, bz], -1)
-
-        # compute diff
-        b_diff_error = torch.clip(torch.abs(b - b_true) - b_err, 0)
-        b_diff_error = torch.mean(torch.nansum(b_diff_error.pow(2), -1).pow(0.5))
-
-        b_diff = torch.abs(b - b_true)
-        b_diff = torch.mean(torch.nansum(b_diff.pow(2), -1).pow(0.5))
-
-        return b_diff, b_diff_error
+        return b, b_true

nf2/train/module.py
@@ -15,8 +15,7 @@ class NF2Module(LightningModule):
                  lambda_energy_gradient=None, lambda_potential=None,
                  lr_params={"start": 5e-4, "end": 5e-5, "iterations": 1e5},
                  meta_path=None,
-                 use_positional_encoding=False, use_vector_potential=False,
-                 coordinate_transform=None, **kwargs):
+                 use_positional_encoding=False, use_vector_potential=False, **kwargs):
         """
         The main module for training the neural field model.
 
@@ -41,14 +40,6 @@ class NF2Module(LightningModule):
             model = VectorPotentialModel(3, dim, pos_encoding=use_positional_encoding)
         else:
             model = BModel(3, 3, dim, pos_encoding=use_positional_encoding)
-        # init coordinate transform modules
-        if coordinate_transform is None:
-            self.coordinate_transform = None
-        elif coordinate_transform == 'z_height':
-            self.coordinate_transform = HeightMappingModel(3, dim)
-        else:
-            raise ValueError(f"Invalid coordinate transform: {coordinate_transform}, must be in [None, 'z_height']")
-
         # load meta state
         if meta_path:
             state_dict = torch.load(meta_path)['model'].state_dict() \
@@ -59,10 +50,10 @@ class NF2Module(LightningModule):
         # init boundary loss module
         if boundary_loss == "azimuth":
             boundary_loss_module = AzimuthBoundaryLoss
-            self.validation_b_diff_metric = AziDiffMetric()
+            self.b_transform = AzimuthTransform()
         elif boundary_loss == "ptr":
             boundary_loss_module = BoundaryLoss
-            self.validation_b_diff_metric = BDiffMetric()
+            self.b_transform = SphericalTransform()
         else:
             raise ValueError(f"Invalid boundary loss: {boundary_loss}, must be in ['azimuth', 'rtp']")
         # mapping
@@ -99,13 +90,10 @@ class NF2Module(LightningModule):
         #
         self.use_vector_potential = use_vector_potential
         self.validation_outputs = {}
-        self.vector_weight = nn.Parameter(torch.tensor([1, 1, 1], dtype=torch.float32).reshape((1, 3)))
         self.loss_modules = nn.ModuleDict(loss_modules)
 
     def configure_optimizers(self):
         parameters = list(self.model.parameters())
-        if self.coordinate_transform is not None:
-            parameters += list(self.coordinate_transform.parameters())
         if isinstance(self.lr_params, dict):
             lr_start = self.lr_params['start']
             lr_end = self.lr_params['end']
@@ -128,9 +116,6 @@ class NF2Module(LightningModule):
         b_err = boundary_batch['errors'] if 'errors' in boundary_batch else 0
 
         boundary_coords.requires_grad = True
-        original_coords = boundary_coords
-        if self.coordinate_transform is not None:
-            boundary_coords = self.coordinate_transform(boundary_coords, boundary_batch['height_ranges'])
 
         random_coords = batch['random']
         random_coords.requires_grad = True
@@ -150,7 +135,7 @@ class NF2Module(LightningModule):
             "n_boundary_coords": n_boundary_coords,
             "boundary_coords": coords[:n_boundary_coords], "random_coords": coords[n_boundary_coords:],
             "boundary_b": b[:n_boundary_coords],  "random_b": b[n_boundary_coords:],
-            "jac_matrix": jac_matrix, "original_coords": original_coords,
+            "jac_matrix": jac_matrix,
         }
         loss_dict = {k: module(**state_dict) for k, module in self.loss_modules.items()}
         total_loss = sum([self.lambdas[k] * loss_dict[k] for k in loss_dict.keys()])
@@ -185,10 +170,17 @@ class NF2Module(LightningModule):
             b_err = batch['errors'] if 'errors' in batch else 0
             boundary_coords.requires_grad = True
             transform = batch['transform'] if 'transform' in batch else None
-            if self.coordinate_transform:
-                boundary_coords = self.coordinate_transform(boundary_coords, batch['height_ranges'])
+
             b = self.model(boundary_coords)
-            b_diff, b_diff_error = self.validation_b_diff_metric(b, b_true, b_err=b_err, transform=transform)
+            b, b_true = self.b_transform(b, b_true, transform=transform)
+
+            # compute diff
+            b_diff_error = torch.clip(torch.abs(b - b_true) - b_err, 0)
+            b_diff_error = torch.mean(torch.nansum(b_diff_error.pow(2), -1).pow(0.5))
+
+            b_diff = torch.abs(b - b_true)
+            b_diff = torch.mean(torch.nansum(b_diff.pow(2), -1).pow(0.5))
+
             return {'b_diff_error': b_diff_error.detach(), 'b_diff': b_diff.detach(),
                     'b': b.detach(), 'b_true': b_true.detach(), 'coords': boundary_coords.detach()}
         else:
@@ -279,14 +271,13 @@ class NF2Module(LightningModule):
 
         self.load_state_dict(state_dict, strict=False)
 
-def save(save_path, model, data_module, config, coordinate_transform_module=None):
+def save(save_path, model, data_module, config):
     save_state = {'model': model,
                   'cube_shape': data_module.cube_shape,
                   'b_norm': data_module.b_norm,
                   'spatial_norm': data_module.spatial_norm,
                   'meta_data': data_module.meta_data,
                   'config': config,
-                  'height_mapping_model': coordinate_transform_module,
                   'height_mapping': data_module.height_mapping if hasattr(data_module, 'height_mapping') else None,
                   'Mm_per_pixel': data_module.Mm_per_pixel if hasattr(data_module, 'Mm_per_pixel') else None,
                   }